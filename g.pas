1d893c461dcf464fa0baef82fae8758f6ab747b7
0renessans0 <144677876+0renessans0@users.noreply.github.com>
2023-09-11 21:29:36 +0300

    Add files via upload

---
+++ g.pas
@@ -0,0 +1,16 @@
+﻿ program f7;
+var a,b,c,g,s,r:integer;
+begin
+ writeln('введите a');
+  readln(a);
+  writeln('введите b');
+  readln(b);
+  writeln('введите c');
+  readln(c);
+  writeln('введите g');
+  readln(g);
+  s:=a+b+c+g;
+  writeln('введите сумму',s);
+  r:=a*b*c*g;
+  writeln('ввeдите разность',r);
+end.
