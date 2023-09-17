64b598623658001456c01b5dbf8b92ddf641a15d
0renessans0 <144677876+0renessans0@users.noreply.github.com>
2023-09-17 22:13:23 +0300

    Add files via upload

---
+++ 3,14.pas
@@ -0,0 +1,11 @@
+﻿var a, b, c:integer; 
+begin
+writeln('введите 3 числа');
+readln(a,b,c);
+if (a < b) and (b < c) then write(a, ' ', b, ' ', c);
+if (a < c) and (c < b) then write(a, ' ', c, ' ', b);
+if (b < a) and (a < c) then write(b, ' ', a, ' ', c);
+if (b < c) and (c < a) then write(b, ' ', c, ' ', a);
+if (c < b) and (b < a) then write(c, ' ', b, ' ', a);
+if (c < a) and (a < b) then write(c, ' ', a, ' ', b);
+end.
---
+++ 3.15.pas
@@ -0,0 +1,10 @@
+﻿var a,b,c,k:integer;
+begin
+k:=0;
+writeLn('Введите числa a,b,c');
+readLn(a,b,c);
+if a > 0 then k:=k + 1;
+if b > 0 then k:=k + 1;
+if c > 0 then k:=k + 1;
+WriteLn('Количество положительных чисел в исходном наборе:',k);
+end.
