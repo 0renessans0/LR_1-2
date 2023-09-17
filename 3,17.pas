7b0409b40f6febdfcde7848e89439766e1ab1cf1
0renessans0 <144677876+0renessans0@users.noreply.github.com>
2023-09-17 21:32:55 +0300

    Add files via upload

---
+++ 3,17.pas
@@ -0,0 +1,24 @@
+﻿var a,b,c,d,x1,x2:real;
+begin
+  writeln(a,b,c);
+  d:=(d*d-4*a*c);
+  readln(a,b,c);
+  writeln(x1,x2);
+  if d<0 then writeln('корней нет')
+  else  if d=0 then
+     begin
+    x1:=-b/2/a;
+    writeln('x1=x2=',x1:0:2);
+   end
+else
+  begin
+   x1:=(-b-sqrt(d))/2/a;
+   x2:=(-b+sqrt(d))/2/a;
+   writeln('x1=',x1:0:2,'   x2=',x2:0:2);
+  end;
+if a>0 then writeln('Ветви параболы направлены вверх')
+else writeln('Ветви параболы направлены вниз');
+readln
+end.
+
+   
---
+++ 4,19.pas
@@ -0,0 +1,16 @@
+﻿var a,b,c,i:integer;
+begin
+  repeat
+write('Введите четырёхзначное целое число');
+readln(a);
+until(a>999)and(a<10000);
+b:=a;
+c:=0;
+for i:=1 to 4 do
+ begin
+  c:=10*c+b mod 10;
+  b:=b div 10;
+ end;
+if c=a then write('палиндром')
+else write('не палиндром');
+end.
