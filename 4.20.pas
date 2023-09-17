6784f4d9ed5dabced75650225cae67f3b4149509
0renessans0 <144677876+0renessans0@users.noreply.github.com>
2023-09-17 22:32:41 +0300

    Add files via upload

---
+++ 4.20.pas
@@ -0,0 +1,8 @@
+﻿var x1,y1,x2,y2: real;
+ begin
+ write('введите координаты');
+ read(x1,y1,x2,y2);
+ if (x1*x2>0)and(y1*y2>0)
+ then write('yes') 
+ else write('no');
+ end.
---
+++ 4.22.pas
@@ -0,0 +1,7 @@
+﻿var x1,x2,y1,y2:integer;
+begin
+writeln('введите координаты лодьи и фигуры');
+readln(x1,x2,y1,y2);
+if (x1=y1) or (x2=y2) then writeln('YES')
+else writeln('NO');
+end.
