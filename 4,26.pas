fdd9c0bf3c824468a81ac7eaa4f464253778f1e4
0renessans0 <144677876+0renessans0@users.noreply.github.com>
2023-09-17 22:46:30 +0300

    Add files via upload

---
+++ 4,26.pas
@@ -0,0 +1,13 @@
+﻿var
+a, b, c: integer;
+begin
+write('введите стороны треугольника');
+readln(a, b, c);
+if not (((a + b) > c) and ((a + c) > b) and ((b + c) > a)) then
+writeln('He существует')
+else
+if (a = b) and (b = c) and (a = c) then
+writeln('Равносторонний')
+else if (a = b) or (b = c) or (a = c) then writeln('Равнобедренный')
+else writeln('Разносторонний');
+end.
