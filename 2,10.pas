7e76484d25613075322a6f26336e57e257e1e094
0renessans0 <144677876+0renessans0@users.noreply.github.com>
2023-09-17 21:59:29 +0300

    Add files via upload

---
+++ 2,10.pas
@@ -0,0 +1,10 @@
+﻿program ch;
+var
+n:integer;
+begin
+write ('Введите число: ');
+readln(n);
+n:=n mod 10;
+writeln ('Последняя цифра в числе: ', n);
+readln;
+end.
---
+++ 2.8.pas
@@ -0,0 +1,9 @@
+﻿var n: Integer;
+begin
+  writeln('введите трехзначное число');
+  ReadLn(n);
+  WriteLn(n div 100);
+  writeln('введите четырёхзначное число');
+  ReadLn(n);
+  WriteLn(n div 1000);
+end.
